5608004f394d3f68e51cffbf4f0405be9ce3159c
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:48:58 +0300

    made create user function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3.6
 
 from user import User
+
+
+def create_user(fname, lname, phone, email, password):
+    '''
+    creating a new user
+    '''
+    new_user = User(fname, lname, phone, email, password)
+    return new_user
