efdd1d93395119998e68bfe0dcb60fe13b4f802d
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:21:18 +0300

    defined class behavior of user

--- user-test.py
+++ user-test.py
@@ -2,5 +2,22 @@ import unittest
 import pyperclip
 from user import User
 
+class TestUser(unittest.TestCase):
+    '''
+    This test defines user class behavior
+    '''
+# => 1
+    '''
+    test 1 : check if it's been instatiated correctly
+    '''
 
+    def setUp(self):
+        self.new_user = User("Moringa", "School", "0712345678", "ms@mail.com", "password")
+    
+    def test_init(self):
+        self.assertEqual(self.new_user.first_name, "Moringa")
+        self.assertEqual(self.new_user.last_name, "School")
+        self.assertEqual(self.new_user.phone_number, "0712345678")
+        self.assertEqual(self.new_user.email, "ms@mail.com")
+        self.assertEqual(self.new_user.password, "password")
 
