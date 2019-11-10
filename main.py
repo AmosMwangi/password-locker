10be2dfc72c4bb06b30c9396419a6cbbd46c4ef0
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:51:15 +0300

    made user save function

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 
 from user import User
 
-
+# creating a contact
 def create_user(fname, lname, phone, email, password):
     '''
     creating a new user
     '''
     new_user = User(fname, lname, phone, email, password)
     return new_user
+
+# save contact
+
+
+def save_users(user):
+    '''
+    save new user
+    '''
+    user.save_user()
+
+# delete contact
