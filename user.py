6b5d1ac36a33a0f5a6fa69c58653487dd565f4f4
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:37:25 +0300

    used the decorator and pyperclip to copy users info like mail.

--- user.py
+++ user.py
@@ -53,3 +53,8 @@ class User:
         return user list
         '''
         return cls.user_list
+
+    @classmethod
+    def copy_email(cls, first_name):
+        user_found = User.find_by_first_name(first_name)
+        pyperclip.copy(user_found.email)
