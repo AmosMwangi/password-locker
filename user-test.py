b74d12d1598f813da8120be10e7562fb5484269b
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:23:26 +0300

    made a test for new user

--- user-test.py
+++ user-test.py
@@ -14,10 +14,4 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Moringa", "School", "0712345678", "ms@mail.com", "password")
     
-    def test_init(self):
-        self.assertEqual(self.new_user.first_name, "Moringa")
-        self.assertEqual(self.new_user.last_name, "School")
-        self.assertEqual(self.new_user.phone_number, "0712345678")
-        self.assertEqual(self.new_user.email, "ms@mail.com")
-        self.assertEqual(self.new_user.password, "password")
-
+    
