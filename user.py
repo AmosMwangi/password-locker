f285a9222e311a13d5dd0ea5c5fa3de4abaf39a2
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:13:55 +0300

    made user class and made __init__ function

--- user.py
+++ user.py
@@ -1,1 +1,16 @@
 import pyperclip
+
+class User:
+    '''
+    user class
+    '''
+
+    user_list = []
+
+    def __init__(self, first_name, last_name, number, email, password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+        self.password = password
