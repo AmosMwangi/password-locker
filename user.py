2c1c00d7996a3a77a1dcad53e8656834f2864cc7
AmosMwangi <amosmwangi101@gmail.com>
2019-11-10 15:27:16 +0300

    made save function that saves user to list--i mean a temporary one--

--- user.py
+++ user.py
@@ -14,3 +14,9 @@ class User:
         self.phone_number = number
         self.email = email
         self.password = password
+
+    def save_user(self):
+        '''
+        saves users into user_list
+        '''
+        User.user_list.append(self)
